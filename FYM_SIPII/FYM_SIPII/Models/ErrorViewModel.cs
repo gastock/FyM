bb2d04fa3cb45b129fe6167378ef1b120ca0b260
GastonStockli <gastons315@gmail.com>
2018-06-02 17:01:17 -0300

    Proyecto creado

---
+++ FYM_SIPII/FYM_SIPII/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FYM_SIPII.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
